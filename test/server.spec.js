f6c776fd55e31a35c840c3b8c96c60510cc1d3ba
mharzi <m.mharzi@gmail.com>
2017-03-30 11:16:33 +0200

    ihm city and filtre

--- test/server.spec.js
+++ test/server.spec.js
@@ -4,29 +4,29 @@ const expect = require('expect');
 const request = require('supertest');
 const app = require('../server');
 
-describe('the server', () => {
-  it('should return 404 on unknown url', done => {
+describe('the server', function() {
+  it('should return 404 on unknown url', function done() {
     request(app)
       .get('/nowhere')
       .expect(404, done);
   });
 
-  it('should return expected JSON on start', done => {
+  it('should return expected JSON on start', function done () {
     const expectedCities = ['Manchester', 'Liverpool', 'York', 'Las Vegas', 'Beijing'];
 
     request(app)
       .get('/visits')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end((err, res) => {
+      .end(function(err, res) {
         const result = res.body;
         expect(result.length).toBe(5);
-        expect(result.map(r => r.City)).toEqual(expectedCities);
+        expect(result.map(r.City)).toEqual(expectedCities);
         done();
       });
   });
 
-  it('should return html at the root url', done => {
+  it('should return html at the root url', function done() {
     request(app)
       .get('/')
       .expect('Content-Type', /html/)
